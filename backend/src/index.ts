ce10ca34caf05042f9efd222ebd1ece862a329bf
Eric Moon <lunar.6022@gmail.com>
2021-11-24 21:58:37 +1100

    Replace aws-serverless-express with @vendia/serverless-express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,6 @@
 export {};
 
-const awsServerlessExpress = require('aws-serverless-express');
+const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./app');
 
-const server = awsServerlessExpress.createServer(app);
-
-exports.handler = (event: any, context: any) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
-};
+exports.handler = serverlessExpress({ app })
