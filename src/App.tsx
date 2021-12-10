18d7479cb60a95b5f5e6a95d03525adb0d91145c
Eric Moon <lunar.6022@gmail.com>
2021-12-10 20:53:53 +1100

    WP-50 Set up Jest in frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
-import './App.css';
 import './fonts/fonts.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
 import { ThemeProvider } from 'styled-components';
 
-import Button from './components/Button';
 import theme from './theme';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <MUIThemeProvider theme={theme}>
       <CssBaseline />
-      <Button text="epic stuff" />
     </MUIThemeProvider>
   </ThemeProvider>
 );
