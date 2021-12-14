2f78a38927d9c93670f18f2d5e21d07df34d0fea
Eric Moon <lunar.6022@gmail.com>
2021-12-15 00:22:27 +1100

    WP-53 Adjust PageLayout styles

--- src/components/PageLayout/index.styles.ts
+++ src/components/PageLayout/index.styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const LoadingContainer = styled.div`
-  margin: auto;
-  min-height: 100vh;
+  height: 100vh;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
