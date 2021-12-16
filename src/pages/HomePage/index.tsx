b94a3105cbf5da18c4c982afa76593a04323f636
Eric Moon <lunar.6022@gmail.com>
2021-12-16 14:41:45 +1100

    WP-36 Remove stub sign out button in HomePage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 
-import { useNavigate } from 'react-router-dom';
-
-import { signOut } from '../../auth';
-import Button from '../../components/Button';
 import { ProtectedPageLayout } from '../../components/PageLayout';
 
 // Stub home page
-const HomePage = () => {
-  const navigate = useNavigate();
-
-  return (
-    <ProtectedPageLayout>
-      <Button onClick={async () => {
-        await signOut();
-        navigate('/signin');
-      }}
-      >Sign Out
-      </Button>
-    </ProtectedPageLayout>
-  );
-};
+const HomePage = () => (
+  <ProtectedPageLayout>
+    Hello world
+  </ProtectedPageLayout>
+);
 export default HomePage;
