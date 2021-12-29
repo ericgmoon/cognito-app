2b64b2f4bc31c8b2ac9feacb1e5077d9d0e6db8e
Eric Moon <lunar.6022@gmail.com>
2021-12-29 12:35:27 +1100

    WP-58 Add gray to Button color options

--- src/types/mui-material/Button.d.ts
+++ src/types/mui-material/Button.d.ts
@@ -3,8 +3,9 @@ import '@mui/material/Button';
 declare module '@mui/material/Button' {
   export interface ButtonPropsColorOverrides {
     darkPrimary: true;
-    darkGray: true,
-    chemistry: true,
-    physics: true,
+    gray: true;
+    darkGray: true;
+    chemistry: true;
+    physics: true;
   }
 }
