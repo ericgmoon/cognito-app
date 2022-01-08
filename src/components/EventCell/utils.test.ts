49bba84c580f2057b5ddef5efa2868a0c7e31038
Eric Moon <lunar.6022@gmail.com>
2022-01-08 16:46:14 +1100

    WP-58 Update expected test data

--- src/components/EventCell/utils.test.ts
+++ src/components/EventCell/utils.test.ts
@@ -2,11 +2,11 @@ import { stringifyTime } from './utils';
 
 describe('stringifyDatetime', () => {
   test('should return correct strings', () => {
-    expect(stringifyTime(new Date(2021, 12, 25, 23, 59, 59, 0).getTime())).toBe('11:59 pm');
-    expect(stringifyTime(new Date(2021, 12, 25, 11, 30, 30, 0).getTime())).toBe('11:30 am');
+    expect(stringifyTime(new Date(2021, 12, 25, 23, 59, 59, 0).getTime())).toBe('11:59 PM');
+    expect(stringifyTime(new Date(2021, 12, 25, 11, 30, 30, 0).getTime())).toBe('11:30 AM');
   });
 
   test('should return 12:00 am if dateless', () => {
-    expect(stringifyTime(new Date(2021, 12, 25).getTime())).toBe('12:00 am');
+    expect(stringifyTime(new Date(2021, 12, 25).getTime())).toBe('12:00 AM');
   });
 });
