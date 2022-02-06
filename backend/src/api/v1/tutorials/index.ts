6c1c59c377276c052cb47bd128e973bbf707b445
ericgmoon <lunar.6022@gmail.com>
2022-02-06 15:19:20 +1100

    WP-74 Add /unbook endpoint

--- backend/src/api/v1/tutorials/index.ts
+++ backend/src/api/v1/tutorials/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import {
-  book, create, get, remove, root,
+  book, create, get, remove, root, unbook,
 } from './endpoints';
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.route('/create').post(create);
 router.route('/get').get(get);
 router.route('/remove').delete(remove);
 router.route('/book').put(book);
+router.route('/unbook').put(unbook);
 
 export { router };
