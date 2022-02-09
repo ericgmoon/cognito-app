01fb381e3d0fd981b685ef1f78f7f90b4f08bae8
ericgmoon <lunar.6022@gmail.com>
2022-02-09 21:01:29 +1100

    WP-77 Remove console statements

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -9,10 +9,8 @@ export const validateNewUser = async (phoneNumber: string | undefined) => {
   if (phoneNumber) {
     try {
       const response = await axios(`${process.env.REACT_APP_BACKEND_URI}users/validate`, { params: { phoneNumber } });
-      console.log(response);
       return response;
     } catch (err) {
-      console.log(err);
       if (axios.isAxiosError(err)) throw Error(err?.response?.data?.message);
       else throw err;
     }
