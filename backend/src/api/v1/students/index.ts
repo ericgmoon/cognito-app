38041ee61f8121053dfcab7d21161c54d1d37d5b
ericgmoon <lunar.6022@gmail.com>
2022-02-14 12:32:15 +1100

    WP-81 Add protect() wrappers

--- backend/src/api/v1/students/index.ts
+++ backend/src/api/v1/students/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import protect from '../../../middleware/protect';
+
 import { getRoot } from './endpoints';
 
 const router = express.Router();
 
-router.route('/:gradYear/:studentId').get(getRoot);
+router.route('/:gradYear/:studentId').get(protect(getRoot));
 
 export { router };
