c718aed64557c9e4e027844fff7c9d17de9fcf2c
ericgmoon <lunar.6022@gmail.com>
2022-03-22 14:35:03 +1100

    WP-95 Expose Cognito user ID and groups

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import { ProtectedPageLayout, TitleWrapper } from '../../components/PageLayout';
 import WeekCalendar from '../../components/WeekCalendar';
+import { getCurrentUserGroups } from '../../services/auth';
+
+getCurrentUserGroups().then((groups) => {
+  console.log(groups);
+});
 
 const HomePage = () => (
   <ProtectedPageLayout title="Home">
