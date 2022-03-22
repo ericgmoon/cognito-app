8d9498f8bfd70740ad6175282956297dea08c714
ericgmoon <lunar.6022@gmail.com>
2022-03-22 14:35:34 +1100

    WP-95 Remove debug statements

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 import { ProtectedPageLayout, TitleWrapper } from '../../components/PageLayout';
 import WeekCalendar from '../../components/WeekCalendar';
-import { getCurrentUserGroups } from '../../services/auth';
-
-getCurrentUserGroups().then((groups) => {
-  console.log(groups);
-});
 
 const HomePage = () => (
   <ProtectedPageLayout title="Home">
